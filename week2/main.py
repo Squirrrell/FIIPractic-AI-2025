fc1e5ef913bc45bf2006192e6d11d0905f19d44b
DenisGithub3 <denisdobos43@gmail.com>
2025-03-29 12:29:01 +0200

    finished week2

--- week2/main.py
+++ week2/main.py
@@ -6,10 +6,11 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
-from src.knn import knn_predict, plot_misclassified_points
+from src.knn import knn_predict, plot_misclassified_points , plot_classified_points
 
-df = pd.read_csv("data/dataset_hipertensiune.csv")
-X = df[["IMC", "Colesterol"]].values
+df = pd.read_csv("data/dataset_hipertensiune2.csv")
+X = df [["Varsta" , "IMC" , "Colesterol"]].values
+# X = df[["IMC", "Colesterol" ]].values
 y = df["Hipertensiune"].values
 
 """
@@ -23,8 +24,18 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-knn_y_pred = knn_predict(X_train, y_train, X_test, k=5)
-knn_accuracy = accuracy_score(y_test, knn_y_pred)
-print(f"Precizia KNN: {knn_accuracy:.2f}")
+"""
+for k in range(1, 52, 2):
+#k numarul de vecini
+
+    knn_y_pred = knn_predict(X_train, y_train, X_test , k )
+    knn_accuracy = accuracy_score(y_test, knn_y_pred)
+    print(f"Precizia KNN: {knn_accuracy:.5f} ({k} vecini)")
+    print(f" ")
+"""
+knn_y_pred = knn_predict(X_train, y_train, X_test , k = 3 )
+
 plot_misclassified_points(X_test, y_test, knn_y_pred)
+
+plot_classified_points(X_train, X_test, y_train, y_test)
 plt.show()
--- week2/src/knn.py
+++ week2/src/knn.py
@@ -1,11 +1,13 @@
 import numpy as np
-from src.utils import euclidean_distance
+from src.utils import euclidean_distance , manhattan_distance , minkowski_distance
 import matplotlib.pyplot as plt
 
 def knn_predict (x_train , y_train , X_test , k=5):
     y_pred = []
     for test_sample in X_test:
         distances = [euclidean_distance(test_sample, x_train) for x_train in x_train]
+        #distances = [manhattan_distance(test_sample, x_train) for x_train in x_train]
+        #distances = [minkowski_distance(test_sample, x_train) for x_train in x_train]
         k_indices = np.argsort(distances)[:k]
         k_nearest_labels = [y_train[i] for i in k_indices]
         most_common = max(set(k_nearest_labels), key=k_nearest_labels.count)
@@ -22,10 +24,25 @@ def plot_misclassified_points(X_test, y_test, y_pred):
        else:
            plt.scatter(X_test[i, 0], X_test[i, 1], color='red', marker='x', alpha=0.6,
                        label='Gresit' if i == 0 else "")
-   plt.xlabel("IMC")
+   plt.xlabel("IMC , Varsta")
    plt.ylabel("Colesterol")
    plt.title("Clasificare corectă vs incorectă")
    plt.legend()
    plt.savefig("clasificare_corecta_vs_incorecta.png")
 
 
+def plot_classified_points(X_train, X_test, y_train, y_test):
+    y_train_symbols = ['o' if label == 0 else 's' for label in y_train]
+    y_test_symbols = ['o' if label == 0 else 's' for label in y_test]
+
+    plt.figure(figsize=(8, 6))
+    for i, symbol in enumerate(y_train_symbols):
+        plt.scatter(X_train[i, 0], X_train[i, 1], marker=symbol, color='blue', alpha=0.5,
+                    label='Train' if i == 0 else "")
+    for i, symbol in enumerate(y_test_symbols):
+        plt.scatter(X_test[i, 0], X_test[i, 1], marker=symbol, color='red', alpha=0.5, label='Test' if i == 0 else "")
+    plt.xlabel("IMC , Varsta")
+    plt.ylabel("Colesterol")
+    plt.title("Clasificarea punctelor (Train vs Test)")
+    plt.legend()
+    plt.savefig("clasificarea_punctelor.png")
